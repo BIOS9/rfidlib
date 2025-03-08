a8d03feb5c0cac55a3376d930c46b59d9551236b
NightFish <NightFish.net@gmail.com>
2025-03-08 20:16:22 +1300

    feat: add basic proxmark3 wrapper for testing

--- src/main/kotlin/bios9/pm3wsl/Pm3CommandException.kt
+++ src/main/kotlin/bios9/pm3wsl/Pm3CommandException.kt
@@ -1,8 +1,7 @@
 package bios9.pm3wsl
 
-class Pm3CommandException(command: String, exitCode: Int, output: String) :
-    RuntimeException("Command '$command' failed with exit code $exitCode:\n$output") {
-    val failedCommand = command
-    val errorCode = exitCode
-    val errorOutput = output
-}
+class Pm3CommandException(
+    command: String,
+    exitCode: Int,
+    output: String
+) : RuntimeException("Command '$command' failed with exit code $exitCode:\n$output")
