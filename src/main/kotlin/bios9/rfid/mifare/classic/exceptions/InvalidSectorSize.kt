39bbf471c1618b494dd9590cb5057998f299697c
NightFish <NightFish.net@gmail.com>
2025-03-10 23:24:58 +1300

    nfc: remove unused exception class

--- src/main/kotlin/bios9/rfid/mifare/classic/exceptions/InvalidSectorSize.kt
+++
@@ -1,6 +0,0 @@
-package bios9.rfid.mifare.classic.exceptions
-
-class InvalidSectorSize(
-    expected: Int,
-    actual: Int
-) : RuntimeException("Invalid MIFARE classic sector size. Expected = $expected, actual = $actual")
--- src/main/kotlin/bios9/rfid/mifare/mad/MifareApplicationDirectory.kt
+++ src/main/kotlin/bios9/rfid/mifare/mad/MifareApplicationDirectory.kt
@@ -2,7 +2,6 @@ package bios9.rfid.mifare.mad
 
 import bios9.rfid.mifare.classic.MifareClassic
 import bios9.rfid.mifare.classic.MifareKeyType
-import bios9.rfid.mifare.classic.exceptions.InvalidSectorSize
 import bios9.rfid.mifare.mad.exceptions.*
 
 /**
