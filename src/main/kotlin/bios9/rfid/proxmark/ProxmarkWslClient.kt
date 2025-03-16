dbd558b4be6a879f76dff7dd53889f4060781641
NightFish <NightFish.net@gmail.com>
2025-03-16 15:07:19 +1300

    feat: implement pm3 wrapper mifare classic auth check

--- src/main/kotlin/bios9/rfid/proxmark/ProxmarkWslClient.kt
+++ src/main/kotlin/bios9/rfid/proxmark/ProxmarkWslClient.kt
@@ -3,7 +3,8 @@ package bios9.rfid.proxmark
 class ProxmarkWslClient(
     private val wslDistro: String? = null, // WSL distribution to use, NULL to use default
     private val pm3Path: String = "~/proxmark3/client/proxmark3", // The path to the **compiled** proxmark 3 client
-    private val pm3Port: String = "/dev/ttyACM0" // Proxmark serial port within WSL2
+    private val pm3Port: String = "/dev/ttyACM0", // Proxmark serial port within WSL2
+    private val debug: Boolean
 ) : ProxmarkClient {
     override fun runCommand(command: String): String {
         val wslCommand = mutableListOf("wsl")
@@ -19,11 +20,19 @@ class ProxmarkWslClient(
         wslCommand.addAll(
             listOf("--", pm3Path, "-p", pm3Port, "-f", "--incognito", "-c", "\"$command\"")
         )
+
+        if (debug) {
+            println("PM3 command:\n$wslCommand")
+        }
         val process = ProcessBuilder(wslCommand).redirectErrorStream(true).start()
         process.waitFor()
 
         val exitCode = process.exitValue()
         val output = process.inputReader().readText();
+
+        if (debug) {
+            println("PM3 output:\n$output")
+        }
 
         if (exitCode != 0) {
             throw Pm3CommandException(command, exitCode, output)
