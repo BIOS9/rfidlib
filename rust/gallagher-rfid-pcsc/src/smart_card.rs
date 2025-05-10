15cdda221bd86551859305c1339983b633ba72b7
BIOS9 <nightfish.net@gmail.com>
2025-05-11 01:42:17 +1200

    feat: add pcsc vendor check for acr reader

--- rust/gallagher-rfid-pcsc/src/smart_card.rs
+++ rust/gallagher-rfid-pcsc/src/smart_card.rs
@@ -11,6 +11,15 @@ impl SmartCard {
         SmartCard { pcsc_card }
     }
 
+    pub fn get_vendor(&mut self) -> Result<String, SmartCardError> {
+        let result = self.pcsc_card.get_attribute_owned(pcsc::Attribute::VendorName);
+        match result {
+            Ok(v) => Ok(String::from_utf8_lossy(&v).to_string()),
+            Err(err) => Err(SmartCardError::CardCommunicateFailed(format!(
+                "Failed to query reader vendor: {}", err)))
+        }
+    }
+
     pub fn control(&mut self, control_code: u32, command: &[u8]) -> Result<Vec<u8>, SmartCardError> {
         let mut response_buff= [0; MAX_BUFFER_SIZE];
         match self.pcsc_card.control(control_code, &command, &mut response_buff) {
@@ -24,13 +33,8 @@ impl SmartCard {
     }
 
     pub fn transmit_apdu(&mut self, apdu: &[u8]) -> Result<Vec<u8>, SmartCardError> {
-        let tx = self.pcsc_card.transaction()
-            .map_err(|err| SmartCardError::CardCommunicateFailed(format!(
-                "Failed to start card transaction: {}", err
-            )))?;
-
         let mut response_buff = [0u8; MAX_BUFFER_SIZE];
-        let response = tx.transmit(apdu, &mut response_buff)
+        let response = self.pcsc_card.transmit(apdu, &mut response_buff)
             .map_err(|err| SmartCardError::CardCommunicateFailed(format!(
                 "Failed to transceive card APDU: {}", err
             )))?;
