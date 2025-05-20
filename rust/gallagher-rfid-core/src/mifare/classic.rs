2a67e4a6a4d32f6c35c1594c9de125c07431009f
NightFish <NightFish.net@gmail.com>
2025-05-20 23:42:37 +1200

    feat: add more conversions to mifare classic blocks/sectors, and add tests

--- rust/gallagher-rfid-core/src/mifare/classic.rs
+++ rust/gallagher-rfid-core/src/mifare/classic.rs
@@ -1,13 +1,34 @@
+use core::fmt;
 use std::{string::String};
 
 /// Represents a valid MIFARE Classic sector from 0 to 39.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct MifareClassicSector(u8);
 
-/// Converts a `MifareClassicSector` into a u8 sector address.
+impl fmt::Display for MifareClassicSector {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Sector {}", self.0)
+    }
+}
+
+/// Converts a `MifareClassicSector` into a `u8` sector address.
 impl From<MifareClassicSector> for u8 {
     fn from(value: MifareClassicSector) -> Self {
         value.0
+    }
+}
+
+/// Attempts conversion of a `u8` into a `MifareClassicSector`.
+/// 
+/// Valid values for a sector are 0..=39
+impl TryFrom<u8> for MifareClassicSector {
+    type Error = MifareClassicError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0..=39 => Ok(MifareClassicSector(value)),
+            _ => Err(MifareClassicError::InvalidSector(value))
+        }
     }
 }
 
@@ -36,6 +57,21 @@ pub struct MifareClassicBlock(u8);
 impl From<MifareClassicBlock> for u8 {
     fn from(value: MifareClassicBlock) -> Self {
         value.0
+    }
+}
+
+impl fmt::Display for MifareClassicBlock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Block {}", self.0)
+    }
+}
+
+/// Converts a `u8` into a `MifareClassicBlock`.
+/// 
+/// Note: All `u8` values are valid since there are 256 blocks starting from 0 in MIFARE Classic.
+impl From<u8> for MifareClassicBlock {
+    fn from(value: u8) -> Self {
+        MifareClassicBlock(value)
     }
 }
 
@@ -81,11 +117,13 @@ pub trait MifareClassic {
 /// Represents errors that can occur during MIFARE Classic operations.
 #[derive(Debug)]
 pub enum MifareClassicError {
-    /// Failed to authenticate to a block.
+    /// Failed to authenticate to a sector.
     AuthenticationFailed {
-        block: u8,
-        reason: String,
+        block: u8
     },
+
+    /// Invalid value for a MIFARE classic sector.
+    InvalidSector(u8),
 
     /// Low-level PCSC or transport error.
     TransportError(String),
@@ -96,4 +134,71 @@ pub enum MifareClassicError {
 /// Used to abstract how keys are retrieved and applied for authentication.
 pub trait MifareClassicKeyProvider {
   fn authenticate<T: MifareClassic>(tag: T, sector: MifareClassicSector);
+}
+
+#[test]
+fn block_to_sector(){
+    // 1k sectors
+    assert_eq!(0u8, MifareClassicSector::from(MifareClassicBlock::from(0)).into());
+    assert_eq!(0u8, MifareClassicSector::from(MifareClassicBlock::from(1)).into());
+    assert_eq!(0u8, MifareClassicSector::from(MifareClassicBlock::from(2)).into());
+    assert_eq!(0u8, MifareClassicSector::from(MifareClassicBlock::from(3)).into());
+    assert_eq!(1u8, MifareClassicSector::from(MifareClassicBlock::from(4)).into());
+    assert_eq!(1u8, MifareClassicSector::from(MifareClassicBlock::from(5)).into());
+    assert_eq!(1u8, MifareClassicSector::from(MifareClassicBlock::from(6)).into());
+    assert_eq!(1u8, MifareClassicSector::from(MifareClassicBlock::from(7)).into());
+    assert_eq!(15u8, MifareClassicSector::from(MifareClassicBlock::from(60)).into());
+    assert_eq!(15u8, MifareClassicSector::from(MifareClassicBlock::from(61)).into());
+    assert_eq!(15u8, MifareClassicSector::from(MifareClassicBlock::from(62)).into());
+    assert_eq!(15u8, MifareClassicSector::from(MifareClassicBlock::from(63)).into());
+
+    // 4k sectors
+    assert_eq!(31u8, MifareClassicSector::from(MifareClassicBlock::from(124)).into());
+    assert_eq!(31u8, MifareClassicSector::from(MifareClassicBlock::from(125)).into());
+    assert_eq!(31u8, MifareClassicSector::from(MifareClassicBlock::from(126)).into());
+    assert_eq!(31u8, MifareClassicSector::from(MifareClassicBlock::from(127)).into());
+    for b in 128..=143 {
+      assert_eq!(32u8, MifareClassicSector::from(MifareClassicBlock::from(b)).into())
+    }
+    assert_eq!(33u8, MifareClassicSector::from(MifareClassicBlock::from(144)).into());
+    assert_eq!(38u8, MifareClassicSector::from(MifareClassicBlock::from(239)).into());
+    for b in 240..=255 {
+      assert_eq!(39u8, MifareClassicSector::from(MifareClassicBlock::from(b)).into());
+    }
+
+    for b in 0..=255 {
+        // Just making sure it doesnt panic.
+        _ = MifareClassicSector::from(MifareClassicBlock::from(b));
+    }
+}
+
+#[test]
+fn sector_to_block() -> Result<(), MifareClassicError> {
+    // 1k sectors
+    assert_eq!(0u8, MifareClassicBlock::from(MifareClassicSector::try_from(0)?).into());
+    assert_eq!(4u8, MifareClassicBlock::from(MifareClassicSector::try_from(1)?).into());
+    assert_eq!(8u8, MifareClassicBlock::from(MifareClassicSector::try_from(2)?).into());
+    assert_eq!(12u8, MifareClassicBlock::from(MifareClassicSector::try_from(3)?).into());
+    assert_eq!(60u8, MifareClassicBlock::from(MifareClassicSector::try_from(15)?).into());
+
+    // 4k sectors
+    assert_eq!(124u8, MifareClassicBlock::from(MifareClassicSector::try_from(31)?).into());
+    assert_eq!(128u8, MifareClassicBlock::from(MifareClassicSector::try_from(32)?).into());
+    assert_eq!(144u8, MifareClassicBlock::from(MifareClassicSector::try_from(33)?).into());
+    assert_eq!(160u8, MifareClassicBlock::from(MifareClassicSector::try_from(34)?).into());
+    assert_eq!(240u8, MifareClassicBlock::from(MifareClassicSector::try_from(39)?).into());
+
+    for b in 0..=39 {
+        // Just making sure it doesnt panic.
+        _ = MifareClassicBlock::from(MifareClassicSector::try_from(b)?);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn invalid_sector() {
+    for i in 40..=255 {
+        assert!(MifareClassicSector::try_from(i).is_err());
+    }
 }
