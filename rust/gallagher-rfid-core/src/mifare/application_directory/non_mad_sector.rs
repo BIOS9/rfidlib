c41915df8c6cddd424beea6fdc2e8c50647ec04a
NightFish <NightFish.net@gmail.com>
2025-05-24 18:21:26 +1200

    feat: add iterator implementation for sectors

--- rust/gallagher-rfid-core/src/mifare/application_directory/non_mad_sector.rs
+++ rust/gallagher-rfid-core/src/mifare/application_directory/non_mad_sector.rs
@@ -1,3 +1,4 @@
+use crate::mifare::application_directory::MadVersion;
 use crate::mifare::classic::Sector;
 
 /// Mifare Application Directory (MAD) Card Publisher Sector (CPS) newtype validation wrapper.
@@ -11,16 +12,18 @@ pub struct CardPublisherSector(Sector);
 impl CardPublisherSector {
     /// Constructs a `CardPublisherSector`, validating the input against known constraints.
     ///
-    /// Returns an error if:
-    /// 1. The CPS cannot be 0x00, this sector is reserved for version 1 of the Mifare Application Directory (MAD).
-    /// 2. The CPS cannot be 0x10, this sector is reserved for version 2 of the Mifare Application Directory (MAD).
-    /// 3. The CPS cannot be greater than 0x27, because the highest sector in Mifare Classic is 39 (0x27).
+    /// Constraints (Error returned if not satisfied):
+    /// 1. The CPS cannot be 0, this sector is reserved for version 1 of the MIFARE Application Directory (MAD).
+    /// 2. The CPS cannot be 16, this sector is reserved for version 2 of the MIFARE Application Directory (MAD).
+    /// 3. The CPS cannot be greater than 39, because that's the highest sector in MIFARE Classic (0x27).
+    /// 4. The CPS cannot be greater than 15 if the MAD version is 1.
     pub fn new(sector: Sector) -> Result<Self, CardPublisherSectorError> {
         match sector.into() {
-            0x00 => Err(CardPublisherSectorError::ReservedForMadV1),
-            0x10 => Err(CardPublisherSectorError::ReservedForMadV2),
-            0x28..=u8::MAX => Err(CardPublisherSectorError::SectorOutOfRange(sector)),
-            _ => Ok(CardPublisherSector(sector)),
+            0 => Err(CardPublisherSectorError::ReservedForMadV1),
+            16 => Err(CardPublisherSectorError::ReservedForMadV2),
+            0..40 => Ok(CardPublisherSector(sector)),
+            // Sector type cannot be greater than 39.
+            _ => unreachable!("Sector had a value greater than 39 {}", sector),
         }
     }
 }
@@ -39,7 +42,8 @@ impl From<CardPublisherSector> for u8 {
 
 #[derive(Debug)]
 pub enum CardPublisherSectorError {
+    /// Sector is reserved for MADv1 data.
     ReservedForMadV1,
+    /// Sector is reserved for MADv2 data.
     ReservedForMadV2,
-    SectorOutOfRange(Sector),
 }
