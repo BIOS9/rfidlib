5be6690fd9217c6a58da1843f82dc13541d5e51d
NightFish <NightFish.net@gmail.com>
2025-05-25 15:23:37 +1200

    fix: remove unused modules

--- rust/gallagher-rfid-core/src/lib.rs
+++ rust/gallagher-rfid-core/src/lib.rs
@@ -3,22 +3,4 @@
 #[cfg(feature = "std")]
 extern crate std;
 
-pub mod error;
 pub mod mifare;
-pub mod transport;
-pub use transport::RfidTransport;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(all(test, feature = "std"))]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
