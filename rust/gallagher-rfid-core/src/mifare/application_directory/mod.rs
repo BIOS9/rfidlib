cd821e92b0f340bf0c96e22d41a1e1e9ecaca7a6
NightFish <NightFish.net@gmail.com>
2025-05-31 22:17:05 +1200

    feat: add example of reading mad

--- rust/gallagher-rfid-core/src/mifare/application_directory/mod.rs
+++ rust/gallagher-rfid-core/src/mifare/application_directory/mod.rs
@@ -1,7 +1,13 @@
-mod non_mad_sector;
 mod mad_application_id;
 mod mifare_application_directory;
+mod non_mad_sector;
 
+pub use mad_application_id::AdministrationCode;
+pub use mad_application_id::FunctionCluster;
+pub use mad_application_id::MadAid;
+pub use mad_application_id::MadAidError;
 pub use mifare_application_directory::MadError;
 pub use mifare_application_directory::MadVersion;
 pub use mifare_application_directory::MifareApplicationDirectory;
+pub use non_mad_sector::NonMadSector;
+pub use non_mad_sector::NonMadSectorError;
